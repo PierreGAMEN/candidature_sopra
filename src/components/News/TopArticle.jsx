82f3287ab60a353ae0802ff32a3bf7618fb73323
Pierre GAMEN <pierre.gamen@gmail.com>
2024-07-15 21:22:21 +0200

    update 'TopArticle)  a => Link

--- src/components/News/TopArticle.jsx
+++ src/components/News/TopArticle.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 import "./topArticle.scss";
 
 const TopArticle = ({image, altImage, date, title, excerpt}) => {
@@ -9,11 +10,11 @@ const TopArticle = ({image, altImage, date, title, excerpt}) => {
       <div className="container_article_img">
       <img src={image} alt={altImage} />
       </div>
-      <a href="/article/top10" className="container_article_content">
+      <Link to="/article/top10" className="container_article_content">
         <h3>{date}</h3>
         <h4>{title}</h4>
         <p>{excerpt}</p>
-      </a>
+      </Link>
     </article>
   );
 };
