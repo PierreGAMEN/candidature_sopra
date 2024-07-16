f1a7602ee3aea3c8d84d6b88331d573ad62b9ba1
Pierre GAMEN <pierre.gamen@gmail.com>
2024-07-16 11:31:36 +0200

    update (main) : add new pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import App from './components/Layout/App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
-import Article1 from './components/Article1/Article1.jsx';
+import Article1 from './components/DetailedArticle/Article1/Article1.jsx';
+import Article2 from './components/DetailedArticle/Article2/Article2.jsx';
+import Article3 from './components/DetailedArticle/Article3/Article3.jsx';
+import Article4 from './components/DetailedArticle/Article4/Article4.jsx';
+
 
 const router = createBrowserRouter([
   {
@@ -12,6 +16,9 @@ const router = createBrowserRouter([
       children: [
           { index: true, element: <Home /> },
           { path: "/article/top10", element: <Article1 /> },
+          { path: "/article/motivations", element: <Article2 /> },
+          { path: "/article/alliance", element: <Article3 /> },
+          { path: "/article/skills", element: <Article4 /> },
       ],
   },
 ]);
