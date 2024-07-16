47035209f75b86ea31ea7cf4741b8ef98cc7b822
Pierre GAMEN <pierre.gamen@gmail.com>
2024-07-16 14:40:26 +0200

    fix header style

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,20 +1,18 @@
+import { Link } from "react-router-dom";
 import "./header.scss";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header_secondary"></div>
       <div className="header_primary">
         <div>
-          <a href="/">
+          <Link to="/">
             <img
               src="https://www.soprasteria.com/ResourcePackages/Bootstrap4/assets/dist/logos/logo-soprasteria.png"
               alt="logo site sopra steria"
             />
-          </a>
+          </Link>
         </div>
-        <div></div>
-        <div></div>
       </div>
     </header>
   );
